7c294fd0ab2c335c9b0647c9b2c9b601f74b2d88
PatternPro <104513893+PatternPro@users.noreply.github.com>
2022-05-25 13:15:41 -0600

    ;

--- js/MainJS.js
+++ js/MainJS.js
@@ -5,14 +5,27 @@ var button3 = document.getElementById("Subpg13")
 var button4 = document.getElementById("Subpg14")
 var button5 = document.getElementById("Subpg15")
 
+var button6 = document.getElementById("Subpg21")
+var button7 = document.getElementById("Subpg22")
+var button8 = document.getElementById("Subpg23")
+var button9 = document.getElementById("Subpg24")
+
 button1.onmouseover(button1.style.width = "100px", button1.style.height = "100px");
 button1.onmouseout(button1.style.width = "95px", button1.style.height = "95px");
-
 button2.onmouseover(button2.style.width = "100px", button2.style.height = "100px");
 button2.onmouseout(button2.style.width = "95px", button2.style.height = "95px");
-
 button3.onmouseover(button3.style.width = "100px", button3.style.height = "100px");
 button3.onmouseout(button3.style.width = "95px", button3.style.height = "95px");
+button4.onmouseover(button4.style.width = "100px", button4.style.height = "100px");
+button4.onmouseout(button4.style.width = "95px", button4.style.height = "95px");
+button5.onmouseover(button5.style.width = "100px", button5.style.height = "100px");
+button5.onmouseout(button5.style.width = "95px", button5.style.height = "95px");
 
-button4.onmouseover(button4.style.width)
-button4.onmouseover(button4.style.width)
+button6.onmouseover(button.style.width = "100px", button.style.height = "100px")
+button6.onmouseout(button.style.width = "95px", button.style.height = "95px")
+button7.onmouseover(button.style.width = "100px", button.style.height = "100px")
+button7.onmouseout(button.style.width = "95px", button.style.height = "95px")
+button8.onmouseover(button.style.width = "100px", button.style.height = "100px")
+button8.onmouseout(button.style.width = "95px", button.style.height = "95px")
+button9.onmouseover(button.style.width = "100px", button.style.height = "100px")
+button9.onmouseout(button.style.width = "95px", button.style.height = "95px")
