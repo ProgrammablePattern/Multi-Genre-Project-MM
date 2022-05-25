a1296ea64dc057877628cf6198cda8ed6d64df7b
PatternPro <104513893+PatternPro@users.noreply.github.com>
2022-05-25 13:45:15 -0600

    ;

--- js/MainJS.js
+++ js/MainJS.js
@@ -1,13 +1,15 @@
-var button1 = document.getElementById("SubPg11")
-var button2 = document.getElementById("SubPg12")
-var button3 = document.getElementById("SubPg13")
-var button4 = document.getElementById("SubPg15")
+var button1 = document.getElementById("SubPg11");
+var button2 = document.getElementById("SubPg12");
+var button3 = document.getElementById("SubPg13");
+var button4 = document.getElementById("SubPg15");
 
-var button5 = document.getElementById("SubPg21")
-var button6 = document.getElementById("SubPg22")
-var button7 = document.getElementById("SubPg23")
-var button8 = document.getElementById("SubPg24")
-var button9 = document.getElementById("SubPg25")
+var button5 = document.getElementById("SubPg21");
+var button6 = document.getElementById("SubPg22");
+var button7 = document.getElementById("SubPg23");
+var button8 = document.getElementById("SubPg24");
+var button9 = document.getElementById("SubPg25");
+
+var FinalButton = document.getElementById("FinalButtonButton");
 
 button1.onmouseover(button1.style.width = "100px", button1.style.height = "100px");
 button1.onmouseout(button1.style.width = "95px", button1.style.height = "95px");
@@ -18,13 +20,16 @@ button3.onmouseout(button3.style.width = "95px", button3.style.height = "95px");
 button4.onmouseover(button4.style.width = "100px", button5.style.height = "100px");
 button4.onmouseout(button4.style.width = "95px", button5.style.height = "95px");
 
-button5.onmouseover(button5.style.width = "100px", button.style.height = "100px")
-button5.onmouseout(button5.style.width = "95px", button.style.height = "95px")
-button6.onmouseover(button6.style.width = "100px", button.style.height = "100px")
-button6.onmouseout(button6.style.width = "95px", button.style.height = "95px")
-button7.onmouseover(button7.style.width = "100px", button.style.height = "100px")
-button7.onmouseout(button7.style.width = "95px", button.style.height = "95px")
-button8.onmouseover(button8.style.width = "100px", button.style.height = "100px")
-button8.onmouseout(button8.style.width = "95px", button.style.height = "95px")
-button9.onmouseover(button9.style.width = "100px", button9.style.height = "100px")
-button9.onmouseout(button9.style.width = "95px", button9.style.height = "95px")
+button5.onmouseover(button5.style.width = "100px", button.style.height = "100px");
+button5.onmouseout(button5.style.width = "95px", button.style.height = "95px");
+button6.onmouseover(button6.style.width = "100px", button.style.height = "100px");
+button6.onmouseout(button6.style.width = "95px", button.style.height = "95px");
+button7.onmouseover(button7.style.width = "100px", button.style.height = "100px");
+button7.onmouseout(button7.style.width = "95px", button.style.height = "95px");
+button8.onmouseover(button8.style.width = "100px", button.style.height = "100px");
+button8.onmouseout(button8.style.width = "95px", button.style.height = "95px");
+button9.onmouseover(button9.style.width = "100px", button9.style.height = "100px");
+button9.onmouseout(button9.style.width = "95px", button9.style.height = "95px");
+
+FinalButton.onmouseover(FinalButton.style.width = "100px", FinalButton.style.height = "100px");
+FinalButton.onmouseout(FinalButton.style.width = "95px", FinalButton.style.height = "95px");
