59ec63f13d6300b8382872c06b37281d71d152ca
PatternPro <104513893+PatternPro@users.noreply.github.com>
2022-05-25 13:07:16 -0600

    Changes

--- js/MainJS.js
+++ js/MainJS.js
@@ -1,1 +1,18 @@
 // 
+var button1 = document.getElementById("Subpg11")
+var button2 = document.getElementById("Subpg12")
+var button3 = document.getElementById("Subpg13")
+var button4 = document.getElementById("Subpg14")
+var button5 = document.getElementById("Subpg15")
+
+button1.onmouseover(button1.style.width = "100px", button1.style.height = "100px");
+button1.onmouseout(button1.style.width = "95px", button1.style.height = "95px");
+
+button2.onmouseover(button2.style.width = "100px", button2.style.height = "100px");
+button2.onmouseout(button2.style.width = "95px", button2.style.height = "95px");
+
+button3.onmouseover(button3.style.width = "100px", button3.style.height = "100px");
+button3.onmouseout(button3.style.width = "95px", button3.style.height = "95px");
+
+button4.onmouseover(button4.style.width)
+button4.onmouseover(button4.style.width)
